636afb51bb010c7efdb491c7b0ee9d0d39cb8d1d
noolisainathareddy <nooli.sainathareddy@gmail.com>
2022-12-25 13:05:33 -0600

    List Interface example

---
+++ ListInterface/src/main/java/com/collections/list/ListDemo.java
@@ -0,0 +1,12 @@
+package com.collections.list;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class ListDemo {
+    public static void main(){
+        List<Integer> arrayList = new ArrayList<>(); 
+
+    }
+}
+
